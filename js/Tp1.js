393d167095bd9ff42126329492febb348a5c4730
Jennifer Alvarez <jennifer.s.alvarez93@gmail.com>
2022-07-01 01:41:20 -0300

    hasta ejercicio numero 5

--- js/Tp1.js
+++ js/Tp1.js
@@ -7,3 +7,12 @@ document.write("<h1>Hello World</h1>");
 
 //punto 3
 document.write("resultado: " + (3 + 5));
+
+//punto 4
+let nombre = prompt("Nombre de Usuario");
+document.write("</br> Hola " + nombre);
+
+//punto 5
+let numero1 = parseInt (prompt ("Primer numero"))
+let numero2 = parseInt (prompt ("Segundo numero"))
+document.write ("<p>Resultado: " + (numero1 + numero2) + "</p>")
